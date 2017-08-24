4ad74132eaacbf7de7de7156c7d47e4c8596bbf6
shehroz-abid <shehroz.abid@gmail.com>
2017-08-24 22:00:16 +0500

    Updated App 24/8/2017

---
+++ app/src/main/java/com/example/lorrynwinsh/Fragments/VehicleTypeFragment.java
@@ -0,0 +1,63 @@
+package com.example.lorrynwinsh.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.lorrynwinsh.R;
+
+/**
+ * Created by Shehroz on 23-Aug-17.
+ */
+
+public class VehicleTypeFragment extends Fragment implements View.OnClickListener{
+
+    LinearLayout visa_layout_btn, cash_layout_btn, paypal_layout_btn;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.vehicle_type, container, false);
+        //((MainActivity)getActivity()).setTopBarTitle("Add Education");
+        //((MainActivity) getActivity()).hideActionBarSearchEditButton();
+
+
+        visa_layout_btn =(LinearLayout) v.findViewById(R.id.visa_layout_btn);
+        cash_layout_btn =(LinearLayout) v.findViewById(R.id.cash_layout_btn);
+        paypal_layout_btn =(LinearLayout) v.findViewById(R.id.paypal_layout_btn);
+
+        visa_layout_btn.setOnClickListener(this);
+        cash_layout_btn.setOnClickListener(this);
+        paypal_layout_btn.setOnClickListener(this);
+        //Typeface typeface = Typeface.createFromAsset(getActivity().getApplicationContext().getAssets(), "fonts/MindBlue_regular_demo.otf");
+        //login_btn.setTypeface(typeface);
+
+        return v;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == visa_layout_btn){
+
+
+        }
+        if(v == cash_layout_btn){
+
+
+        }
+        if(v == paypal_layout_btn){
+
+
+        }
+
+
+        // implements your things
+    }
+}
+
